9f427d662c23880e23deabb2a2eed1e0ae55cff7
Paul Franz <paulkfranz@gmail.com>
2021-05-10 16:35:20 -0400

    sourcelist updated per guidelines

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ var config = {
     title: 'The Climate Factor in Nigeria’s Farmer-Herder Violence',
     subtitle: '',
     byline: '',
-    footer: '<ul><li><strong>Lead Contributor</strong>: Ulrich Eberle</li><li><strong>Visualizations</strong>: Paul Franz</li></ul><br /><em>Empirical analysis is based on Eberle et al. (2020) “Heat and Hate: Climate Security and Farmer-Herder Conflicts in Africa”</em><br /><br /><strong>Data Sources</strong>: ACLED, CCI-LC (ESA), CRU, GADM, WRI, OpenStreetMap, Mapbox<br /><br /><img src="images/crisis_group_dark_logo.png" width="150" height="150" />',
+    footer: '<ul><li><strong>Lead Contributor</strong>: Ulrich Eberle</li><li><strong>Visualizations</strong>: Paul Franz</li></ul><br /><em>Empirical analysis is based on Eberle, U. J,, D. Rohner and M. Thoenig. 2020 “Heat and Hate: Climate Security and Farmer-Herder Conflicts in Africa”. </em><a href="https://esoc.princeton.edu/WP22" target="_blank">ESOC Working Paper 22</a>.<br /><br /><strong>Data Sources</strong>:<br /><br /> <a href="http://www.esa-landcover-cci.org/?q=node/164" target="_blank">European Space Agency</a>. 2017. Land Cover CCI Product User Guide Version 2. <br /><br /><a href="https://gadm.org/" target="_blank">GADM</a>. 2012. “Database of Global Administrative Areas” <br /><br />Jones, P. and I. Harris. 2008. “CRU Time Series (TS) High Resolution Gridded Datasets v. 4.05." <a href="https://crudata.uea.ac.uk/cru/data/hrg/" target="_blank">Climate Research Unit</a>. <br /><br />Luck, M., M. Landis, and F. Gassert. 2015. "Aqueduct Water Stress Projections: Decadal Projections of Water Supply and Demand using CMIP5 GCMs." <a href="https://www.wri.org/applications/aqueduct/water-risk-atlas/#/?advanced=false&basemap=hydro&indicator=w_awr_def_tot_cat&lat=-14.445396942837744&lng=-142.85354599620152&mapMode=view&month=1&opacity=0.5&ponderation=DEF&predefined=false&projection=absolute&scenario=optimistic&scope=baseline&timeScale=annual&year=baseline&zoom=2" target="_blank">World Resources Institute</a>.<br /><br /> Raleigh, C., A. Linke, H. Hegre and J. Karlsen. 2010. “<a href="https://acleddata.com/data-export-tool/" target="_blank">Introducing ACLED Armed Conflict Location and Event Data</a>." Journal of Peace Research 47(5):651-660.<br /><br /> <a href="https://www.openstreetmap.org" target="_blank">OpenStreetMap</a>, <a href="https://www.mapbox.com" target="_blank">Mapbox</a><br /><br /><img src="images/crisis_group_dark_logo.png" width="150" height="150" />',
     chapters: [
         {
             id: 'intro',
@@ -53,7 +53,7 @@ var config = {
             hidden: false,
             title: 'Africa’s Farmer-Herder Violence: An Arc of Instability',
             image: '',
-            description: 'Rising temperatures and erratic rainfall have intensified resource competition between farmers and herders, both shifting traditional migratory patterns and exacerbating intercommunal violence. In Nigeria, many herders are from the Fulani people, while most farmers are of other ethnicities.<br /><h4>Total fatalities - land-related violent incidents since 2017</h4><img style="width: auto !important;" src="images/fatalities_vertical_1.png" />',
+            description: 'Rising temperatures and erratic rainfall have intensified resource competition between farmers and herders, both shifting traditional migratory patterns and exacerbating intercommunal violence. In Nigeria, many herders are from the Fulani people, while most farmers are of other ethnicities. (Data: <a href="http://www.acleddata.com/" target="_blank">ACLED</a>)<br /><h4>Total fatalities - land-related violent incidents since 2017</h4><img style="width: auto !important;" src="images/fatalities_vertical_1.png" />',
             location: {
               center: [-3.15195, 4.63389],
               zoom: 3.5,
@@ -91,7 +91,7 @@ var config = {
             hidden: false,
             title: 'Climate Change and Rising Temperatures',
             image: '',
-            description: 'From 1950 to 2010, average annual temperatures across the Sahel and in some neighbouring countries increased by 1°C and average annual rainfall decreased by 4.1cm, outpacing global climate trends on both counts. Here is the eleven-year temperature average in 1955.<h4>Temperature average 1945-1955</h4><div id="wri-values" class="row labels"><div class="label">16.4°C</div><div class="label"> </div>  <div class="label">30.5°C</div></div><div class="temperature-legend-colors"></div>',
+            description: 'From 1950 to 2010, average annual temperatures across the Sahel and in some neighbouring countries increased by 1°C and average annual rainfall decreased by 4.1cm, outpacing global climate trends on both counts. Here is the eleven-year temperature average in 1955. (Data: CRU)<h4>Temperature average 1945-1955</h4><div id="wri-values" class="row labels"><div class="label">16.4°C</div><div class="label"> </div>  <div class="label">30.5°C</div></div><div class="temperature-legend-colors"></div>',
             location: {
               center: [-3.15195, 4.63389],
               zoom: 3.5,
@@ -123,7 +123,7 @@ var config = {
             hidden: false,
             title: '',
             image: '',
-            description: 'Now here is the eleven-year temperature average in 2010.<h4>Temperature average 2005-2015</h4><div id="wri-values" class="row labels"><div class="label">16.4°C</div><div class="label"> </div>  <div class="label">30.5°C</div></div><div class="temperature-legend-colors"></div>',
+            description: 'Now here is the eleven-year temperature average in 2010. (Data: CRU)<h4>Temperature average 2005-2015</h4><div id="wri-values" class="row labels"><div class="label">16.4°C</div><div class="label"> </div>  <div class="label">30.5°C</div></div><div class="temperature-legend-colors"></div>',
             location: {
               center: [-3.15195, 4.63389],
               zoom: 3.5,
@@ -154,7 +154,7 @@ var config = {
             hidden: false,
             title: 'Climate Change Increases Economic Uncertainty',
             image: '',
-            description: 'Global warming has not only increased the severity of droughts, but also contributed to extreme seasonal variability in water supply across the Sahel and neighbouring countries. These long-term climatic trends disrupt and harm traditional livelihoods like farming and herding, increasing economic uncertainty.<h4>Seasonal variability in water supply (1950-2010 average)</h4><div id="wri-values" class="row labels"><div class="label">< Low</div><div class="label"> </div>  <div class="label">High ></div></div><div class="wri-legend-colors"></div>',
+            description: 'Global warming has not only increased the severity of droughts, but also contributed to extreme seasonal variability in water supply across the Sahel and neighbouring countries. These long-term climatic trends disrupt and harm traditional livelihoods like farming and herding, increasing economic uncertainty. (Data: WRI)<h4>Seasonal variability in water supply (1950-2010 average)</h4><div id="wri-values" class="row labels"><div class="label">< Low</div><div class="label"> </div>  <div class="label">High ></div></div><div class="wri-legend-colors"></div>',
             location: {
               center: [-3.15195, 4.63389],
               zoom: 3.5,
@@ -186,7 +186,7 @@ var config = {
             hidden: false,
             title: 'Nigeria’s Agricultural Dependence',
             image: '',
-            description: 'Like many countries in Africa, Nigeria is highly dependent on agriculture. Approximately two thirds of the labour force makes a living through farming or pastoralism (herding). With very little irrigated land, both activities rely heavily on seasonal rainfall and related weather patterns, so the effects of climate change can be intense.<h4>Land use data, 2018</h4><img style="width: auto !important;" src="images/landuse_legend.png" /> ',
+            description: 'Like many countries in Africa, Nigeria is highly dependent on agriculture. Approximately two thirds of the labour force makes a living through farming or pastoralism (herding). With very little irrigated land, both activities rely heavily on seasonal rainfall and related weather patterns, so the effects of climate change can be intense. (Data: CCI-LC (ESA))<h4>Land use data, 2018</h4><img style="width: auto !important;" src="images/landuse_legend.png" /> ',
             location: {
               center: [6.75824, 8.03246],
               zoom: 6,
@@ -217,7 +217,7 @@ var config = {
             hidden: false,
             title: 'Geography of Nigeria’s Farmer-Herder Violence ',
             image: '',
-            description: 'High population growth, the Boko Haram insurgency and cattle rustling have forced herders in the north to migrate toward the Middle Belt in central Nigeria in search of pasture and water. Their movement has inflamed competition over land resources already heightened by increasing climate change and led to more frequent disputes between herders and farmers. Crisis Group’s analysis finds that deadly conflicts between farmers and herders are concentrated in Nigeria’s grasslands and along the agricultural fringe used by both groups – especially when land productivity is very low.<h4>Land use categories most prone to resource competition</h4><img style="width: auto !important;" src="images/conflict_land_use_legend.png" />',
+            description: 'High population growth, the Boko Haram insurgency and cattle rustling have forced herders in the north to migrate toward the Middle Belt in central Nigeria in search of pasture and water. Their movement has inflamed competition over land resources already heightened by increasing climate change and led to more frequent disputes between herders and farmers. Crisis Group’s analysis finds that deadly conflicts between farmers and herders are concentrated in Nigeria’s grasslands and along the agricultural fringe used by both groups – especially when land productivity is very low. (Data: CCI-LC (ESA))<h4>Land use categories most prone to resource competition</h4><img style="width: auto !important;" src="images/conflict_land_use_legend.png" />',
             location: {
               center: [6.75824, 8.03246],
               zoom: 6,
@@ -249,7 +249,7 @@ var config = {
             hidden: false,
             title: 'Nigeria’s Farmer-Herder Violence Trend',
             image: '',
-            description: 'Farmer-herder violence has become a major security problem, killing and displacing thousands of Nigerians. Climate change has aggravated the conflict. In the first half of 2018, farmer-herder violence killed up to six times more people than the Boko Haram insurgency in the country’s North East.<h4>Total fatalities in land-related violent incidents since 2017</h4><img style="width: auto !important;" src="images/fatalities_vertical_2.png" />',
+            description: 'Farmer-herder violence has become a major security problem, killing and displacing thousands of Nigerians. Climate change has aggravated the conflict. In the first half of 2018, farmer-herder violence killed up to six times more people than the Boko Haram insurgency in the country’s North East. (Data: <a href="http://www.acleddata.com/" target="_blank">ACLED</a>)<h4>Total fatalities in land-related violent incidents since 2017</h4><img style="width: auto !important;" src="images/fatalities_vertical_2.png" />',
             location: {
               center: [6.37998, 9.09991],
               zoom: 5.50,
@@ -291,7 +291,7 @@ var config = {
             hidden: false,
             title: 'Nigeria’s Vulnerable Middle Belt Region',
             image: '',
-            description: 'While farmer-herder violence has spread across the country, Nigeria’s Middle Belt is particularly affected. Since 2018, Benue, Kaduna, Plateau and Nasarawa states have witnessed hundreds of clashes, armed attacks on communities or other violence involving farmers and herders.<h4>Total fatalities by state - land-related violence since 2017</h4><img style="width: auto !important;" src="images/heightmap.png" />',
+            description: 'While farmer-herder violence has spread across the country, Nigeria’s Middle Belt is particularly affected. Since 2018, Benue, Kaduna, Plateau and Nasarawa states have witnessed hundreds of clashes, armed attacks on communities or other violence involving farmers and herders. (Data: <a href="http://www.acleddata.com/" target="_blank">ACLED</a>)<h4>Total fatalities by state - land-related violence since 2017</h4><img style="width: auto !important;" src="images/heightmap.png" />',
             location: {
                 center: [9.030198, 8.195849],
                 zoom: 6.70,
@@ -462,7 +462,7 @@ var config = {
             hidden: false,
             title: 'Nasarawa’s Deadly Escalation',
             image: '',
-            description: 'From January to June 2018, over 260 people were killed in several incidents, mostly in Nasarawa’s southern zone covering Doma, Awe, Obi and Keana local government areas. In May 2018, federal authorities deployed a military force, Operation Whirl Stroke, to Nasarawa and three additional states. The violence has ebbed for now in Nasarawa.<h4>Total fatalities - violence in Nasarawa (January-June 2018)</h4><img style="width: auto !important;" src="images/fatalities_vertical_3.png" />',
+            description: 'From January to June 2018, over 260 people were killed in several incidents, mostly in Nasarawa’s southern zone covering Doma, Awe, Obi and Keana local government areas. In May 2018, federal authorities deployed a military force, Operation Whirl Stroke, to Nasarawa and three additional states. The violence has ebbed for now in Nasarawa. (Data: <a href="http://www.acleddata.com/" target="_blank">ACLED</a>)<h4>Total fatalities - violence in Nasarawa (January-June 2018)</h4><img style="width: auto !important;" src="images/fatalities_vertical_3.png" />',
             location: {
                 center: [8.348450, 8.645048],
                 zoom: 8,
@@ -700,7 +700,7 @@ var config = {
             hidden: false,
             title: 'Reducing Risks of Violence',
             image: '',
-            description: 'The federal government’s plan is a positive step but will not by itself end risks of farmer-herder violence exacerbated by climate change &dash; even if fully implemented. Planned ranch and grazing reserve locations may themselves be vulnerable to climate-related threats, such as growing water scarcity, over coming decades. The federal government should ensure that all states participating in the livestock transformation plan adopt measures to address climate change’s impact and consider future climate projections in designating locations for ranches and grazing reserves.<h4>2030 forecast - seasonal variability, water supply</h4><div id="wri-values" class="row labels"><div class="label">< Low</div><div class="label"> </div>  <div class="label">High ></div></div><div class="wri-legend-colors"></div>',
+            description: 'The federal government’s plan is a positive step but will not by itself end risks of farmer-herder violence exacerbated by climate change &dash; even if fully implemented. Planned ranch and grazing reserve locations may themselves be vulnerable to climate-related threats, such as growing water scarcity, over coming decades. The federal government should ensure that all states participating in the livestock transformation plan adopt measures to address climate change’s impact and consider future climate projections in designating locations for ranches and grazing reserves. (Data: WRI)<h4>2030 forecast - seasonal variability, water supply</h4><div id="wri-values" class="row labels"><div class="label">< Low</div><div class="label"> </div>  <div class="label">High ></div></div><div class="wri-legend-colors"></div>',
             location: {
               center: [13.398643, 8.313016],
               zoom: 5.50,
